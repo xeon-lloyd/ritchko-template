8b5c7b102789d728f2b2fc28f37b8b9b6bcdd055
Xeon Yuisa Lloyd <jdh10033@naver.com>
2025-08-31 06:10:36 +0900

    add: enum 시스템 추가

---
+++ backend/enums.js
@@ -0,0 +1,14 @@
+/* 지원 이미지 mimeType */
+const ImageMimeType = [
+    'image/jpeg',
+    'image/png',
+]
+
+/* 상수 예시 */
+const SomeConstEnum = 6371;
+
+
+module.exports = {
+    ImageMimeType,
+    SomeConstEnum,
+}
