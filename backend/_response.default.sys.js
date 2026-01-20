7647b6ff61b65f0e698853033cbd47246a2bd972
Xeon Yuisa Lloyd <jdh10033@naver.com>
2026-01-21 03:20:12 +0900

    fix: api 기본응답 스키마 label 속성 제거 (미들웨어에서 자동화)

--- backend/_response.default.sys.js
+++ backend/_response.default.sys.js
@@ -7,86 +7,91 @@ module.exports = {
 
         response = 200;
         message = "";
-        label = null;
         target = null;
         data = null;
     },
 
     BadRequest: class {
-        constructor(label, message) {
-            this.label = label || null;
+        constructor(message) {
             this.message = message || "Bad Request";
         }
 
         response = 400;
         message = "";
-        label = null;
         target = null;
         data = null;
     },
 
     Unauthorized: class {
-        constructor(label, message) {
-            this.label = label || null;
+        constructor(message) {
             this.message = message || "로그인이 필요합니다";
         }
 
         response = 401;
         message = "";
-        label = null;
         target = null;
         data = null;
     },
     
     Forbidden: class {
-        constructor(label, message) {
-            this.label = label || null;
+        constructor(message) {
             this.message = message || "Forbidden";
         }
 
         response = 403;
         message = "";
-        label = null;
         target = null;
         data = null;
     },
 
     NotFound: class {
-        constructor(label, message) {
-            this.label = label || null;
+        constructor(message) {
             this.message = message || "Not found";
         }
 
         response = 404;
         message = "";
-        label = null;
         target = null;
         data = null;
     },
 
     MethodNotAllowed: class {
-        constructor(label, message) {
-            this.label = label || null;
+        constructor(message) {
             this.message = message || "Method Not Allowed";
         }
 
         response = 405;
         message = "";
-        label = null;
+        target = null;
+        data = null;
+    },
+
+    TooManyRequests: class TooManyRequests {
+        response = 429;
+        message = "Too many requests";
         target = null;
         data = null;
     },
 
     InternalServerError: class{
-        constructor(label, message) {
-            this.label = label || null;
+        constructor(message) {
             this.message = message || "Internal Server Error";
         }
 
         response = 500;
         message = "";
-        label = null;
         target = null;
+        data = null;
+    },
+
+    InputValueNotValid: class InputValueNotValid {
+        constructor(target){
+            if(target) this.target = target;
+        }
+
+        response = 400;
+        message = "Input value is not valid"
+        target = '{param}'
         data = null;
     },
 
