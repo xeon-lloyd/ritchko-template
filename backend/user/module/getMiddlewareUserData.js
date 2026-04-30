f1e60a2b1d6f2e49cf283e1cef8e1c868a208e8e
xeon-lloyd <xeon_lloyd@ddhinc.net>
2026-04-30 20:07:14 +0900

    fix: 모듈 파일 컨벤션 준수 및 redis 연결점 오류 수정

--- backend/user/module/getMiddlewareUserData.js
+++ backend/user/module/getMiddlewareUserData.js
@@ -2,11 +2,11 @@ const util = require('../../core/util.js');
 const setting = require('../../core/setting.js');
 
 // 미들웨어에서 사용자 데이터 조회 로직
-module.exports = async function(tokenData){
+module.exports = async function getMiddlewareUserData(tokenData){
     // 캐싱된 사용자 데이터 조회
     // 미들웨어에서의 사용자 캐싱은 token session storage 이용 (토큰 로직으로 취급)
     const cacheKey = `cache:user:${tokenData.uid}`;
-    let userData = await util.token.redisSessionStorage.get(cacheKey);
+    let userData = await util.redis.get(cacheKey);
     if(userData) return JSON.parse(userData);
     
     // 캐싱된 사용자 데이터가 없다면 데이터베이스에서 조회
@@ -27,7 +27,7 @@ module.exports = async function(tokenData){
 
     // 사용자 데이터 캐싱 (10분)
     const USER_CACHE_EXPIRE = 10 * 60;
-    await util.token.redisSessionStorage.set(cacheKey, JSON.stringify(userData), USER_CACHE_EXPIRE);
+    await util.redis.set(cacheKey, JSON.stringify(userData), USER_CACHE_EXPIRE);
 
     return userData;
 }
